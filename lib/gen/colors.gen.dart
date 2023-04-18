b384af17d9b43b092fb50718af8f00278163822f
Konstantin <the.it.promo@gmail.com>
2023-04-18 22:18:39 +0600

    base styles

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,30 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #6188b1
+  static const Color bgBlue = Color(0xFF6188B1);
+
+  /// Color: #e3886a
+  static const Color bgOrange = Color(0xFFE3886A);
+
+  /// Color: #676b8e
+  static const Color darkGrey = Color(0xFF676B8E);
+
+  /// Color: #F5F4F8
+  static const Color lightGrey = Color(0xFFF5F4F8);
+
+  /// Color: #252B5C
+  static const Color primaryColor = Color(0xFF252B5C);
+}
